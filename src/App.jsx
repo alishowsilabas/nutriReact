b1f4b425c90aa7f7d65764e992ea5bacbf833831
alishowsilabas <alissonsilabas@gmail.com>
2023-11-24 13:56:56 -0300

    Fixed Header and tag enhanced

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,46 @@
-import React, { useEffect, useState } from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+
+import './style.css';
+
 // https://sujeitoprogramador.com/rn-api/?api=posts
 
 const App = () => {
-  const [nutri, setNutri] = useState([]);
+	const [nutri, setNutri] = useState([]);
 
-  useEffect(() => {
-    function loadApi() {
-      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+	useEffect(() => {
+		function loadApi() {
+			let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 
-      fetch(url)
-        .then((r) => r.json())
-        .then((json) => {
-          console.log(json);
-          setNutri(json);
-        });
-    }
-    loadApi();
-  }, []);
+			fetch(url)
+				.then((r) => r.json())
+				.then((json) => {
+					console.log(json);
+					setNutri(json);
+				});
+		}
+		loadApi();
+	}, []);
 
-  return (
-    <div className="container">
-      <header>
-        <strong>React Nutri</strong>
-      </header>
+	return (
+		<div className="container">
+			<header className={'myHeader'}>
+				<strong>React Nutri</strong>
+			</header>
 
-      {nutri.map((item) => {
-        return (
-          <article key={item.id} className="post">
-            <strong className="titulo">{item.titulo}</strong>
-            <img
-              src={item.capa}
-              alt="Mesa com alimentos variados, leite, legumes, queijos, etc"
-            />
-            <p className="subtitulo">{item.subtitulo}</p>
-            <a className="contentButton" href="">
-              Acessar conteúdo completo
-            </a>
-          </article>
-        );
-      })}
-    </div>
-  );
+			{nutri.map((item) => {
+				return (
+					<article key={item.id} className={'post'}>
+						<strong className="titulo">{item?.titulo}</strong>
+						<img src={item?.capa} alt="Mesa com alimentos variados, leite, legumes, queijos, etc" />
+						<p className={'subtitulo'}>{item?.subtitulo}</p>
+						<a className={'contentButton'} href="">
+							Acessar conteúdo completo
+						</a>
+					</article>
+				);
+			})}
+		</div>
+	);
 };
 
 export default App;
